aa5cdf3852a610a5ea906ff27863feeeb42cf7cc
Joseph Valdez <joseph.m.valdez1@gmail.com>
2021-09-09 13:28:26 -0700

    Maven added as framework for Jackson api. Fat Jar Created. WIP: Json file is readable and iterable.

--- src/main/java/com/game/controller/Game.java
+++ src/main/java/com/game/controller/Game.java
@@ -4,19 +4,19 @@
 package com.game.controller;
 
 
-import com.game.client.Client;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.game.model.engine.JsonReader;
 import com.game.model.engine.LogicEngine;
 import com.game.model.materials.Caterpillar;
 import com.game.model.materials.Enemy;
 import com.game.model.materials.Location;
 import com.game.view.ViewWindow;
-import com.sun.tools.javac.Main;
 
-import java.io.*;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Game {
     private HashMap<String, Location> locations;
@@ -24,40 +24,45 @@ public class Game {
     private Caterpillar caterpillar;
     private LogicEngine processor;
     private ViewWindow viewWindow;
+
     public Game() {
 
     }
+
     //This should be called by the client to start a new game.
-    public void start(){
+    public void start() {
         setUpComponents();
         run();
     }
 
     //This method is designed to instantiate the necessary fields of a Game object.
-    private void setUpComponents(){
+    private void setUpComponents() {
         this.enemies = populateEnemies();
         this.locations = populateLocations();
-        this.caterpillar = new Caterpillar(100,0,0);
-        this.processor = new LogicEngine(caterpillar,locations, enemies);
+        this.caterpillar = new Caterpillar(100, 0, 0);
+        this.processor = new LogicEngine(caterpillar, locations, enemies);
         this.caterpillar.setCurrentLocation(locations.get("GENESIS"));
         this.viewWindow = new ViewWindow(caterpillar, processor);
     }
+
     //This class controls the game loop. As the user inputs information the view will be updated.
     //I want an instructions panel to be read and you cant start the game until you hit
-    private void run(){
+    private void run() {
         int counter = 0;
         viewWindow.welcomeMessage();
-        while (true){
-                viewWindow.updateCaterpillarStatus();
-                caterpillar.healthRegenerator(counter++);
+        while (true) {
+            viewWindow.updateCaterpillarStatus();
+            caterpillar.healthRegenerator(counter++);
         }
 
     }
 
     //This is a private helper method to read in all of the locations in a text file and parse them to ingame Location objects.
-    private HashMap<String,Location> populateLocations(){
-        HashMap<String,Location> locations = new HashMap<>();
+    private HashMap<String, Location> populateLocations() {
+        HashMap<String, Location> locations = new HashMap<>();
         String[] locationFields;
+        ArrayList<String> rooms = new ArrayList<>();
+        ArrayList<String> descriptions = new ArrayList<>();
         //                locationFields = myReader.nextLine().split(",");
 //
 //                Location loc = new Location(locationFields[0].trim(),locationFields[ 1].trim(), locationFields[ 2].trim(), locationFields[ 3].trim(), locationFields[4].trim(),locationFields[ 5].trim() );
@@ -66,43 +71,73 @@ public class Game {
 //        File file = new File(getClass().getResource("src/com/game/locations.txt").toURI());
 //            Scanner myReader = new Scanner(file);
 //            while(myReader.hasNextLine()){
-        try{
-            InputStream inputStream = getClass().getResourceAsStream("locations.txt");
+        try {
+            String file = "src/main/resources/GrubsLife_Locations.json";
+            JsonNode node = JsonReader.parse(JsonReader.readFileAsString(file));
+
+            Iterator<Map.Entry<String, JsonNode>> nodes = node.get("Locations").fields();
+//            node.at("/Locations").fields().forEachRemaining(entry -> {
+//                System.out.println("Key --> " + entry.getKey() + "\nvalue --> " + entry.getValue());
+//            });
+
+            while (nodes.hasNext()) {
+                Map.Entry<String, JsonNode> entry = (Map.Entry<String, JsonNode>) nodes.next();
+//                System.out.println("entry " + entry);
+                String roomNames = entry.getKey();
+                String roomDescriptions = entry.getValue().get("description").asText();
+                String north = entry.getValue().get("north").asText();
+                String south = entry.getValue().get("south").asText();
+                String east = entry.getValue().get("east").asText();
+                String west = entry.getValue().get("west").asText();
+                rooms.add(roomNames);
+                descriptions.add(roomDescriptions);
+                System.out.println("Key --> " + entry.getKey() + "\nvalue --> " + entry.getValue());
+
+            }
+            System.out.println(rooms);
+            System.out.println(descriptions);
+//-- old stuff start
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("locations.txt");
             InputStreamReader myReader = new InputStreamReader(inputStream);
             BufferedReader br = new BufferedReader(myReader);
             String line = null;
-            while((line = br.readLine() )!= null){
+            while ((line = br.readLine()) != null) {
                 locationFields = line.split(",");
 
-                Location loc = new Location(locationFields[0].trim(),locationFields[ 1].trim(), locationFields[ 2].trim(), locationFields[ 3].trim(), locationFields[4].trim(),locationFields[ 5].trim() );
+                Location loc = new Location(locationFields[0].trim(), locationFields[1].trim(), locationFields[2].trim(), locationFields[3].trim(), locationFields[4].trim(), locationFields[5].trim());
+                // 2 = North, 3 = South, 4 = east, 5 = west
                 loc.setEnemy(enemies.get(locationFields[0].trim().toLowerCase(Locale.ROOT)));
                 locations.put(locationFields[0].trim(), loc);
+                //System.out.println("LOC " + locationFields[2].trim());
             }
-            System.out.println(locations.toString());
+
             br.close();
             myReader.close();
             inputStream.close();
-        }catch (IOException e){
+//--old stuff end
+
+        } catch (IOException e) {
             System.out.println(e.getMessage());
-            }
+        }
         return locations;
     }
+
     //This is a private helper method to populate Enemy objects from an external text file.
-    private HashMap<String,Enemy> populateEnemies(){
+    private HashMap<String, Enemy> populateEnemies() {
         HashMap<String, Enemy> enemies = new HashMap<>();
 
         String[] enemyFields;
-        try{
+        try {
 
 
-            InputStream inputStream = getClass().getResourceAsStream("enemies.txt");
+            InputStream inputStream = getClass().getResourceAsStream("/enemies.txt");
             InputStreamReader myReader = new InputStreamReader(inputStream);
             BufferedReader br = new BufferedReader(myReader);
             String line = null;
-            while((line = br.readLine() )!= null){
+            while ((line = br.readLine()) != null) {
                 enemyFields = line.split(",");
 
-                Enemy enemy = new Enemy(enemyFields[0].trim(),Integer.parseInt(enemyFields[ 1].trim()), Integer.parseInt(enemyFields[ 2].trim()), Integer.parseInt(enemyFields[ 3].trim()), Boolean.parseBoolean(enemyFields[4].trim()), Boolean.parseBoolean(enemyFields[5].trim()), enemyFields[6].trim(), Boolean.parseBoolean(enemyFields[7].trim()));
+                Enemy enemy = new Enemy(enemyFields[0].trim(), Integer.parseInt(enemyFields[1].trim()), Integer.parseInt(enemyFields[2].trim()), Integer.parseInt(enemyFields[3].trim()), Boolean.parseBoolean(enemyFields[4].trim()), Boolean.parseBoolean(enemyFields[5].trim()), enemyFields[6].trim(), Boolean.parseBoolean(enemyFields[7].trim()));
                 enemies.put(enemyFields[6].trim(), enemy);
 
             }
@@ -110,11 +145,12 @@ public class Game {
             br.close();
             myReader.close();
             inputStream.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
         return enemies;
     }
+
     public HashMap<String, Enemy> getEnemies() {
         return enemies;
     }
