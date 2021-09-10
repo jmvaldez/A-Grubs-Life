9967c1d3a0500118a3cab80322541a3369a5982f
Hongyi <qhyprc@gmail.com>
2021-09-10 12:19:07 -0700

    nothing

--- src/com/game/model/newengine/HandelUserInput.java
+++ src/com/game/model/newengine/HandelUserInput.java
@@ -1,15 +1,23 @@
 package com.game.model.newengine;
 
-import com.game.view.ViewWindow;
+import com.game.controller.Game;
 
 public class HandelUserInput {
 
     public HandelUserInput(String input) {
-        while(true){
-        try{
-            String[] actionInput = input.toLowerCase().split(" ");
-            catch(Exception e)
+        while (true) {
+            try {
+                String[] actionInput = input.toLowerCase().split(" ");
 
+                processInput(actionInput);
+                break;
+            } catch (Exception e) {
+                Game.caterpillar.setLastAction("invalid retype");
+            }
+        }
+    }
+
+    public void processInput(String[] input) {
 
 
     }
