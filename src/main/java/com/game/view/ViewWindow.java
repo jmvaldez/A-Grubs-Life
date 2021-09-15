d0e50001c78e4505fafa30f05ff38f38037d5a53
Jauric Flowers <jflow415@gmail.com>
2021-09-15 12:52:59 -0400

    AB#427 created music button to play music during the game

--- src/main/java/com/game/view/ViewWindow.java
+++ src/main/java/com/game/view/ViewWindow.java
@@ -3,10 +3,16 @@ package com.game.view;
 import com.game.controller.Game;
 import com.game.model.materials.Location;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
+import java.io.File;
 
 public class ViewWindow {
 
@@ -31,9 +37,16 @@ public class ViewWindow {
     private JLabel westRoomLabel;
     private JLabel currentRoomLabel;
     private JLabel emptyLabel;
+    private JButton soundButton;
+    private String rickRoll;
+    private String musicOnOff;
+    private ButtonHandler bHandler;
+    private Music mu;
 
 
     public ViewWindow() {
+        bHandler = new ButtonHandler();
+        mu = new Music();
         this.window = new JFrame("A Grub's Life.");
         this.window.setLayout(new BorderLayout());
         this.window.setPreferredSize(new Dimension(1024, 768));
@@ -42,11 +55,85 @@ public class ViewWindow {
 //        this.window.setLocationRelativeTo(null);
         this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.window.pack();
+        setUpSoundButton();
         setUpInputPanel();
         setUpStatPanel();
         setUpDescriptionPanel();
         setUpLocationPanel();
 
+
+    }
+
+    public class Music {
+        Clip clip;
+
+        public void setFile(String soundFileName) {
+
+            try {
+                File file = new File(soundFileName);
+                AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+                clip = AudioSystem.getClip();
+                clip.open(sound);
+            } catch (Exception e) {
+            }
+        }
+
+        public void play() {
+            clip.setFramePosition(0);
+            clip.start();
+        }
+
+        public void loop() {
+            clip.loop(clip.LOOP_CONTINUOUSLY);
+        }
+
+        public void stop() {
+            clip.stop();
+            clip.close();
+        }
+    }
+
+    public class ButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            String clickedButton = event.getActionCommand();
+
+            switch (clickedButton) {
+                case "buttonClick":
+                    if (musicOnOff.equals("off")) {
+                        mu.setFile(rickRoll);
+                        mu.play();
+                        mu.loop();
+                        musicOnOff = "on";
+                        soundButton.setText("Hot Tunes Playing!!!");
+                    } else if (musicOnOff.equals("on")) {
+                        mu.stop();
+                        musicOnOff = "off";
+                        soundButton.setText("No More Hot Tunes");
+                    }
+                    break;
+            }
+        }
+    }
+//    private void menuBar(){
+//        final JMenuBar menuBar = new JMenuBar();
+//
+//        //creating menus
+//        JMenu audioMenu = new JMenu("Audio");
+//
+//        //create menu items
+//        menuBar.add(audioMenu);
+//    }
+
+    private void setUpSoundButton(){
+
+        soundButton = new JButton("Hot Tunes!");
+        soundButton.setPreferredSize(new Dimension(200,50));
+        soundButton.setFocusPainted(false);
+        soundButton.addActionListener(bHandler);
+        soundButton.setActionCommand("buttonClick");
+        rickRoll = "src/main/resources/audio/never.wav";
+        musicOnOff = "off";
+        this.window.add(soundButton,BorderLayout.NORTH);
 
     }
 
@@ -65,6 +152,7 @@ public class ViewWindow {
         inputPanel.add(inputField, BorderLayout.NORTH);
         inputPanel.add(lastMoveLabel, BorderLayout.CENTER);
         this.window.add(inputPanel, BorderLayout.SOUTH);
+
 
 
     }
@@ -113,10 +201,10 @@ public class ViewWindow {
         statPanel.setLayout(new BorderLayout());
 //        statPanel.setLayout(new GridLayout(0, 1));
         statPanel.setBackground(new Color(0, 0, 0));
-        statPanel.setPreferredSize(new Dimension(300,800));
+        statPanel.setPreferredSize(new Dimension(300, 800));
         setCaterpillarStatLabel();
         setEnemyStatLabel();
-        statPanel.add(caterpillarStatLabel,BorderLayout.NORTH);
+        statPanel.add(caterpillarStatLabel, BorderLayout.NORTH);
         statPanel.add(enemyStatLabel, BorderLayout.SOUTH);
 
         this.window.add(statPanel, BorderLayout.EAST);
@@ -240,7 +328,7 @@ public class ViewWindow {
 
         locationPanel.setLayout(new BorderLayout());
         locationPanel.setBackground(new Color(0, 0, 0));
-        locationPanel.setPreferredSize(new Dimension(300,800));
+        locationPanel.setPreferredSize(new Dimension(300, 800));
 
 
         setMapPanel(locationPanel);
@@ -274,13 +362,12 @@ public class ViewWindow {
         mapLabel = new JLabel();
 
 
-
-
         TitledBorder map = new TitledBorder("Map");
         map.setTitleColor(Color.GREEN);
         mapLabel.setBorder(map);
         mapLabel.setPreferredSize(new Dimension(100, 15));
-        locationPanel.add(mapLabel,BorderLayout.NORTH);
+        locationPanel.add(mapLabel, BorderLayout.NORTH);
+
 //
 //        TitledBorder map = new TitledBorder("Map");
 //        map.setTitleColor(Color.GREEN);
